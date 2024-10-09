2d19935a7a7640f89b2f67187defb2f04aba3878
Micheal <micksilwimba867@gmail.com>
2024-10-09 10:33:33 +0200

    js build

---
+++ index.js
@@ -0,0 +1,21 @@
+const link = document.getElementById('share-link');
+const button = document.getElementById("button");
+const button2 = document.getElementById("button2");
+let displayState = false; 
+link.style = 'display: none';
+
+
+const linkDisplay = () => {
+    if (displayState === false) {
+        link.style = 'display: grid';
+        button.style = 'background-color: hsl(217, 19%, 35%)';
+        return displayState = true;
+    } else {
+        link.style = 'display: none';
+        button.style = 'background-color: hsl(212, 23%, 69%)';
+        return displayState = false;
+    }
+};
+
+button.addEventListener('click', linkDisplay);
+button2.addEventListener('click', linkDisplay);
